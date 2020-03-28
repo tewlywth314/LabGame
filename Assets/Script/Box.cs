e81929e9199f175527d9cefba88d0a89ba1124d0
tewlywth314 <61342905+tewlywth314@users.noreply.github.com>
2020-03-28 19:42:07 +0700

    Movement Finished
    Movement Finished

--- Assets/Script/Box.cs
+++ Assets/Script/Box.cs
@@ -5,14 +5,36 @@ using UnityEngine;
 public class Box : MonoBehaviour
 {
     // Start is called before the first frame update
+    public Camera cam;
+    public Transform player;
     public GameObject goal;
+    public float speed = 1.0f;
+    public float Distance;
+    Rigidbody rb;
+    private void Start()
+    {
+        Distance = Vector3.Distance(player.position, cam.transform.position);
+        rb = player.GetComponent<Rigidbody>();
+    }
     private void OnMouseDrag()
     {
+
         Vector3 MousePosWorld = Input.mousePosition;
-        MousePosWorld.z = transform.position.z - Camera.main.transform.position.z;
+
+        MousePosWorld.z = Distance;
+
         MousePosWorld = Camera.main.ScreenToWorldPoint(MousePosWorld);
-        transform.position = new Vector3(MousePosWorld.x,MousePosWorld.y,MousePosWorld.z);
+       
+     
+        rb.velocity = (MousePosWorld - player.position)*2 ;
+        //transform.position = new Vector3(MousePosWorld.x,1.6f,MousePosWorld.z);
+        
     }
+    private void OnMouseUp()
+    {
+        rb.velocity = Vector3.zero;
+    }
+   
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.Equals(goal))
