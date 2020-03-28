bee115ca1ee751c98c83b52f176f30dde2a74c70
tewlywth314 <61342905+tewlywth314@users.noreply.github.com>
2020-03-28 17:18:34 +0700

    Initial commit

---
+++ Assets/Script/Box.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Box : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject goal;
+    private void OnMouseDrag()
+    {
+        Vector3 MousePosWorld = Input.mousePosition;
+        MousePosWorld.z = transform.position.z - Camera.main.transform.position.z;
+        MousePosWorld = Camera.main.ScreenToWorldPoint(MousePosWorld);
+        transform.position = new Vector3(MousePosWorld.x,MousePosWorld.y,MousePosWorld.z);
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.Equals(goal))
+        {
+            Debug.Log("Win");
+        }
+    }
+}
---
+++ Assets/Script/Newscript.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Newscript : MonoBehaviour
+{
+    public Transform startTransform,endTransform,cubeTransform;
+    private bool loop = true;
+    //public float A = 0.5f;
+    
+    
+    //public float Wave(float A)
+    //{
+    //    return 0.5f* Mathf.Sin(Time.time)+0.5f;
+    //}
+
+    // Start is called before the first frame update
+
+    // Update is called once per frame
+    void Start()
+    {
+        
+    }
+    void Update()
+    {
+        
+            cubeTransform.position = Vector3.Lerp(startTransform.position, endTransform.position, 0.5f * Mathf.Sin(Time.time) + 0.5f);
+            
+        
+        
+        
+            
+        
+        
+        
+        
+    }
+}
